dbfbb7f4f6496a5dd6f5babb7a7cd8b2980a3044
Rodrigo Sommacal <rodrigosommacal@gmail.com>
2021-03-01 21:13:32 +0100

    2. chapter

---
+++ challenge-1.php
@@ -0,0 +1,18 @@
+<?php
+  $name= "Teresa"; // Heading 1
+  $city = "NYC";
+  $movie = "Star Wars"; //Italics
+  $friend = "George"; //Bold
+  $candy = "Sour Patch Kids";
+?>
+
+<h1>Hello <?php echo $name;?>!</h1>
+<?php
+//echo "<h1>Hello, $name!</h1>\n\n";
+
+echo "<p>So glad you can join us in $city today! You will be <b>$friend</b>'s guide as you do a range of actitivies.</p>\n\n";
+
+echo "<p>You will end your day by seeing <i>$movie</i> at 7:00pm. Make sure to bring the money you’ve been provided. The movie ticket is \$12. $friend's favorite candy is $candy, so grab some of those too.</p>\n\n";
+
+echo "<p>...and of-course, \"May the Force be with You!\"</p>";
+
